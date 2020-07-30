e1cd534987b252d62b6714dd47991a00da859f6e
adebiyi adedotun <adebiyi@kobo360.com>
2020-07-30 11:49:34 +0100

    Initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+// src/components/Banner.js
+import React from "react"
+
+export default function Banner() {
+  return (
+    <section className="banner">
+      <h1 className="banner-title">
+        This site uses cookies. We don't need you to accept or not.
+      </h1>
+    </section>
+  )
+}
---
+++ src/components/Container.js
@@ -0,0 +1,6 @@
+// src/components/Container.js
+import React from "react"
+
+export default function Container({ children }) {
+  return <div className="container">{children}</div>
+}
